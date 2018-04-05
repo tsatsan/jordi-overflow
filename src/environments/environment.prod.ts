34b8b51143d423819f40723d45655b4cc20acaf6
jordiCostaFigueras <desarrolloweb@worktel.com>
2018-04-05 19:19:37 +0200

    resolving app production issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://lit-hollows-57772.herokuapp.com/api'
+  production: true,
+  apiUrl: 'https://lit-hollows-57772.herokuapp.com/api/'
 };
