8ec237e1ff6e55a8b5e5402ab1acebeb1a9fa7b1
jordiCostaFigueras <desarrolloweb@worktel.com>
2018-04-05 19:08:07 +0200

    resolving issues

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
+
 import http from 'http'
 import Debug from 'debug'
 import app from './app'
-import mongoose, { Mongoose } from 'mongoose'
+import mongoose from 'mongoose'
 import { mongoUrl, port } from './config'
 
+const debug = new Debug('platzi-overflow:root')
 
-const debug = new Debug('jordi-overflow:root')
-
-mongoose. Promise = global.Promise
+mongoose.Promise = global.Promise
 
 async function start() {
-    await mongoose.connect(mongoUrl)
+  await mongoose.connect(mongoUrl, { useMongoClient: true })
 
-app.listen(port, () => {
+  app.listen(port, () => {
     debug(`Server running at port ${port}`)
-    })
+  })
 }
+
 start()
